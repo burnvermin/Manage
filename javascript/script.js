27595af71d112f2ded1f20ac3d97a093b7409276
Oluwagbemiga David <39445905+burnvermin@users.noreply.github.com>
2021-05-24 14:25:44 +0100

    Files Added

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+//Toggling the menu
+var x = document.getElementById("menu");
+var y = document.getElementById('hamburger').style.backgroundImage = "url('../images/icon-hamburger.svg')";
+
+    function menuFunction() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  
+}
+
+//Changing the Hamburger img
+function function2(){
+   if ( x.style.display === "none") {
+          y = document.getElementById('hamburger').style.backgroundImage = "url('../images/icon-hamburger.svg')"; 
+    }else {
+        y = document.getElementById('hamburger').style.backgroundImage = "url('../images/icon-close.svg')"; 
+    }
+    
+}
